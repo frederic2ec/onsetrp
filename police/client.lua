0c05bd3cd53ad8885dc84bce81185111d9957fee
tatou1000 <tatou1000ar@gmail.com>
2019-12-26 02:23:13 -0500

    Police system update: Fix the fine system. Add the garage + armory NPC. Handcuffed players are now unable to enter/leave vehicles. Fix the handcuff system.

--- police/client.lua
+++ police/client.lua
@@ -11,7 +11,10 @@ end)
 
 AddEvent("OnTranslationReady", function()
     policeNpcMenu = Dialog.create(_("police_menu"), nil, _("start_stop_police") ,_("cancel"))
-    policeMenu = Dialog.create(_("police_menu"), nil,  _("spawn_despawn_patrol_car"), _("handcuff_player"), _("put_player_in_vehicle"), _("remove_player_from_vehicle"), _("remove_all_weapons"),_("give_player_fine"), _("cancel"))
+    policeMenu = Dialog.create(_("police_menu"), nil, _("handcuff_player"), _("put_player_in_vehicle"), _("remove_player_from_vehicle"), _("remove_all_weapons"),_("give_player_fine"), _("cancel"))
+    
+    policeNpcGarageMenu = Dialog.create(_("police_garage_menu"), nil,  _("spawn_despawn_patrol_car"), _("cancel"))
+    policeNpcArmoryMenu = Dialog.create(_("police_armory"), nil,  _("get_equipped"), _("cancel"))
 
     policeReceiveFineMenu = Dialog.create(_("fine"), _("fine_price").." : {fine_price} ".._("currency").." | ".._("reason").." : {reason}", _("pay"))
     
@@ -23,10 +26,16 @@ end)
 
 AddEvent("OnKeyPress", function( key )
     if key == "E" and not onSpawn and not onCharacterCreation then
-        local NearestPolice = GetNearestPolice()
-        if NearestPolice ~= 0 then
-            Dialog.show(policeNpcMenu)
-		end
+	local NearestPolice, purpose = GetNearestPolice()
+	if NearestPolice ~= 0 then
+	    if(purpose == "police_job") then
+		Dialog.show(policeNpcMenu)
+	    elseif(purpose == "police_garage") then
+		Dialog.show(policeNpcGarageMenu)
+	    elseif(purpose == "police_armory") then
+		Dialog.show(policeNpcArmoryMenu)
+	    end
+	end
     end
     if key == "F3" and not onSpawn and not onCharacterCreation then
         CallRemoteEvent("OpenPoliceMenu")
@@ -41,23 +50,30 @@ AddEvent("OnDialogSubmit", function(dialog, button, ...)
 	    CallRemoteEvent("StartStopPolice")
 	end
     end
-    if dialog == policeMenu then
+    if dialog == policeNpcGarageMenu then
 	if button == 1 then
 	    CallRemoteEvent("GetPatrolCar")
 	end
-	if button == 2 then
+    end
+    if dialog == policeNpcArmoryMenu then
+	if button == 1 then
+	    CallRemoteEvent("GetEquipped")
+	end
+    end
+    if dialog == policeMenu then
+	if button == 1 then
 	    CallRemoteEvent("HandcuffPlayerSetup")
 	end
-	if button == 3 then
+	if button == 2 then
 	    CallRemoteEvent("PutPlayerInVehicle")
 	end
-	if button == 4 then
+	if button == 3 then
 	    CallRemoteEvent("RemovePlayerOfVehicle")
 	end
-	if button == 5 then
+	if button == 4 then
 	    CallRemoteEvent("RemoveAllWeaponsOfPlayer")
 	end
-	if button == 6 then
+	if button == 5 then
 	    CallRemoteEvent("OpenPoliceFineMenu")
 	end
     end
@@ -94,22 +110,22 @@ AddRemoteEvent("OpenPoliceFineMenu", function(playerNames)
 end)
 
 function GetNearestPolice()
-	local x, y, z = GetPlayerLocation()
-	
-	for k,v in pairs(GetStreamedNPC()) do
-        local x2, y2, z2 = GetNPCLocation(v)
-		local dist = GetDistance3D(x, y, z, x2, y2, z2)
+    local x, y, z = GetPlayerLocation()
 
-		if dist < 250.0 then
-			for k,i in pairs(policeNPC) do
-				if v == i then
-					return v
-				end
-			end
+    for k,v in pairs(GetStreamedNPC()) do
+	local x2, y2, z2 = GetNPCLocation(v)
+	local dist = GetDistance3D(x, y, z, x2, y2, z2)
+
+	if dist < 250.0 then
+	    for k,i in pairs(policeNPC) do
+		if v == i[1] then
+		    return v, i[2]
 		end
+	    end
 	end
+    end
 
-	return 0
+    return 0, ""
 end
 
 AddRemoteEvent("ChangeUniformClient", function(playerToChange, pieceName, part)
@@ -146,6 +162,17 @@ AddEvent("OnPlayerDeath", function(player, instigator)
     end
 end)
 
+AddEvent("OnPlayerStartExitVehicle", function(vehicle)
+    if(GetPlayerPropertyValue(GetPlayerId(), "cuffed")) then
+	return false
+    end
+end)
+
+AddEvent("OnPlayerStartEnterVehicle", function(vehicle)
+    if(GetPlayerPropertyValue(GetPlayerId(), "cuffed")) then
+	return false
+    end
+end)
 
 AddRemoteEvent("PlayerReceiveFine", function(amount, reason)
     Dialog.setVariable(policeReceiveFineMenu, "fine_price", amount)
