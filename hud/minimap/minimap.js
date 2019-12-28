df19781f2770c318014282495fdf136281397553
Soljian <maxverr@gmail.com>
2019-12-28 15:49:31 +0100

    fix désactivation boussole (azimuth)

--- hud/minimap/minimap.js
+++ hud/minimap/minimap.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById("direction-hud");
-var ctx = canvas.getContext("2d");
+// var canvas = document.getElementById("direction-hud");
+// var ctx = canvas.getContext("2d");
 
 function drawTriangle(ctx, x, y, size, color){
     ctx.beginPath();
@@ -11,68 +11,68 @@ function drawTriangle(ctx, x, y, size, color){
     ctx.fill();
 }
 
-let markers = {}
-let lastHeading = 0;
+// let markers = {}
+// let lastHeading = 0;
 
-function SetHUDMarker(name, h, r, g, b){
-    if(h === undefined){
-        markers[name] = undefined;
-    }else{
-        markers[name] = {
-            h: h,
-            r: r,
-            g: g,
-            b: b
-        };
-    }
-}
+// function SetHUDMarker(name, h, r, g, b){
+//     if(h === undefined){
+//         markers[name] = undefined;
+//     }else{
+//         markers[name] = {
+//             h: h,
+//             r: r,
+//             g: g,
+//             b: b
+//         };
+//     }
+// }
 
-let directionNames = {
-    0: "N",
-    45: "NE",
-    90: "E",
-    135: "SE",
-    180: "S",
-    225: "SW",
-    270: "W",
-    315: "NW"
-}
+// let directionNames = {
+//     0: "N",
+//     45: "NE",
+//     90: "E",
+//     135: "SE",
+//     180: "S",
+//     225: "SW",
+//     270: "W",
+//     315: "NW"
+// }
 
-function SetHUDHeading(heading){
-    canvas
-    lastHeading = heading;
-    ctx.clearRect(0, 0, 600, 60);
-    ctx.fillStyle = "rgba(50,50,50,0.3)";
-    ctx.fillRect(0, 0, 600, 60);
-    ctx.fillStyle = "rgba(50,50,50,0.4)";
-    ctx.fillRect(0, 30, 600, 20);
-    ctx.fillStyle = "rgb(255,255,255)";
-    for(let i=0; i<360; i+=15){
-        let h = (i+heading+75) % 360;
-        if(h > 180){
-            h -= 360;
-        }
-        if(i % 45 == 0){
-            ctx.fillRect((h*4)-1, 10, 2, 15);
-            let offset = directionNames[i].length > 1 ? 10 : 4;
-            ctx.font = "bold 15px sans-serif";
-            ctx.fillText(directionNames[i], (h*4)-offset, 45);
-        }else{
-            ctx.fillRect(h*4, 10, 1, 15);
-            let offset = i > 99 ? 9 : (i > 9 ? 6 : 3);
-            ctx.font = "15px sans-serif";
-            ctx.fillText(i, (h*4)-offset, 45);
-        }
-    }
-    for(let name of Object.keys(markers)){
-        let h = (markers[name].h+heading+75) % 360;
-        if(h > 180){
-            h -= 360;
-        }
-        drawTriangle(ctx, h*4, 0, 12, "rgb("+markers[name].r+","+markers[name].g+","+markers[name].b+")");
-    }
-    drawTriangle(ctx, 300, 0, 8, "rgb(255,255,255)");
-}
+// function SetHUDHeading(heading){
+//     canvas
+//     lastHeading = heading;
+//     ctx.clearRect(0, 0, 600, 60);
+//     ctx.fillStyle = "rgba(50,50,50,0.3)";
+//     ctx.fillRect(0, 0, 600, 60);
+//     ctx.fillStyle = "rgba(50,50,50,0.4)";
+//     ctx.fillRect(0, 30, 600, 20);
+//     ctx.fillStyle = "rgb(255,255,255)";
+//     for(let i=0; i<360; i+=15){
+//         let h = (i+heading+75) % 360;
+//         if(h > 180){
+//             h -= 360;
+//         }
+//         if(i % 45 == 0){
+//             ctx.fillRect((h*4)-1, 10, 2, 15);
+//             let offset = directionNames[i].length > 1 ? 10 : 4;
+//             ctx.font = "bold 15px sans-serif";
+//             ctx.fillText(directionNames[i], (h*4)-offset, 45);
+//         }else{
+//             ctx.fillRect(h*4, 10, 1, 15);
+//             let offset = i > 99 ? 9 : (i > 9 ? 6 : 3);
+//             ctx.font = "15px sans-serif";
+//             ctx.fillText(i, (h*4)-offset, 45);
+//         }
+//     }
+//     for(let name of Object.keys(markers)){
+//         let h = (markers[name].h+heading+75) % 360;
+//         if(h > 180){
+//             h -= 360;
+//         }
+//         drawTriangle(ctx, h*4, 0, 12, "rgb("+markers[name].r+","+markers[name].g+","+markers[name].b+")");
+//     }
+//     drawTriangle(ctx, 300, 0, 8, "rgb(255,255,255)");
+// }
 
 var canvas2 = document.getElementById("map-hud");
 var ctx2 = canvas2.getContext("2d");
