4f37d9bb8d662b85897607c790b1202b45a4d997
Rémi Delhaye <remi@slaask.com>
2019-12-30 22:03:53 +0100

    Fix

--- shops/server.lua
+++ shops/server.lua
@@ -63,12 +63,6 @@ end)
 AddRemoteEvent("shopInteract", function(player, shopNpc)
     local shop = GetShopByObject(shopNpc)
 
-    if v.type == "weapons" and PlayerData[player].gun_license == 0 then
-        CallRemoteEvent(player, "MakeNotification", _("no_gun_license"), "linear-gradient(to right, #ff5f6d, #ffc371)")
-    else
-        CallRemoteEvent(player, "openShop", PlayerData[player].inventory, v.items, v.npc[npcid])
-    end
-
     if shop then
 		local x, y, z = GetNPCLocation(shop.npc)
 		local x2, y2, z2 = GetPlayerLocation(player)
