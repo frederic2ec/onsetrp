728a96aec6c0f06f7e67ce9b5a788b48b3781f47
Rémi Delhaye <contact@rdlh.io>
2019-12-30 16:03:52 +0100

    Update inventory displaying

--- shops/client.lua
+++ shops/client.lua
@@ -86,12 +86,12 @@ AddRemoteEvent("openShop", function(inventory, items, shopid)
 	local inventoryitems = {}
 	for k,v in pairs(inventory) do
     if k ~= "cash" then
-		  inventoryitems[k] = _(k).." ["..v.."]"
+		  inventoryitems[k] = v.." x ".._(k)
     end
 	end
 	local shopitems = {}
 	for k,v in pairs(items) do
-		shopitems[k] = _(k).." ["..v.._("currency").."]"
+		shopitems[k] = _(k).." ("..v.._("currency")..")"
 	end
 	Dialog.setSelectLabeledOptions(shop, 1, 1, inventoryitems)
 	Dialog.setSelectLabeledOptions(shop, 2, 1, shopitems)
