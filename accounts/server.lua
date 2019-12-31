26041f2cef0eaaa1b4486ae1aa0a9fbad0b21205
Frédéric C <freddy.cho@hotmail.com>
2019-12-31 16:39:08 -0500

    Update server.lua

--- accounts/server.lua
+++ accounts/server.lua
@@ -163,7 +163,7 @@ function OnAccountLoaded(player)
 			CallRemoteEvent(player, "ClientChangeClothing", player, 1, PlayerData[player].clothing[3], 0, 0, 0, 0)
 			CallRemoteEvent(player, "ClientChangeClothing", player, 4, PlayerData[player].clothing[4], 0, 0, 0, 0)
 			CallRemoteEvent(player, "ClientChangeClothing", player, 5, PlayerData[player].clothing[5], 0, 0, 0, 0)
-			CallRemoteEvent(player, "AskSpawnMenu")
+			-- CallRemoteEvent(player, "AskSpawnMenu")
 		end
 		
 		LoadPlayerPhoneContacts(player)
