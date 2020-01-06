bc89467e10a8330b119453c27214887585b4fa44
Soljian / Sheaven <maxverr@gmail.com>
2020-01-06 18:57:57 +0100

    fix drugknowledge empty

--- accounts/server.lua
+++ accounts/server.lua
@@ -89,7 +89,7 @@ function OnAccountCheckIpBan(player)
 end
 
 function CreatePlayerAccount(player)
-	local query = mariadb_prepare(sql, "INSERT INTO accounts (id, steamid, clothing, clothing_police, death_pos, inventory, position) VALUES (NULL, '?', '[]' , '[]' , '[]' , '[]' , '[]');",
+	local query = mariadb_prepare(sql, "INSERT INTO accounts (id, steamid, clothing, clothing_police, death_pos, inventory, position, drug_knowledge) VALUES (NULL, '?', '[]' , '[]' , '[]' , '[]' , '[]', '[]');",
 		tostring(GetPlayerSteamId(player)))
 
 	mariadb_query(sql, query, OnAccountCreated, player)
