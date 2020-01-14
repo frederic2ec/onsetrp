b5b0833a8f6fb55a1898cfda9dd687a9db496aa5
GalaxHD551 <59099660+GalaxHD551@users.noreply.github.com>
2020-01-14 23:39:10 +0400

    Update tazer.lua

--- misc/tazer.lua
+++ misc/tazer.lua
@@ -4,11 +4,11 @@ function OnTazerShot(player, weapon, hittype, hitid, hitx, hity, hitz, startx, s
 	if (hittype == HIT_PLAYER) then
 		if (weapon == 21) then -- 21 is the weapon ID for the tazer
 			SetPlayerRagdoll(hitid, true) -- Makes player ragdoll
-            Delay(6000, function() -- Waits 6 seconds before the player can stand up again
-                SetPlayerRagdoll(hitid, false) -- Disables the ragdoll so he can walk again.
-                SetPlayerAnimation(player, "PUSHUP_END")    
-		    end)
-	    end        
+            		Delay(6000, function() -- Waits 6 seconds before the player can stand up again
+                		SetPlayerRagdoll(hitid, false) -- Disables the ragdoll so he can walk again.
+                		SetPlayerAnimation(player, "PUSHUP_END")    
+			end)
+	    	end        
 	end
 end
 AddEvent("OnPlayerWeaponShot", OnTazerShot)
