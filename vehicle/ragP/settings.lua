d00e1b9f18354a3e14a62dbd1f717a43b63a6608
Léo Dhn <32024357+Bad57@users.noreply.github.com>
2020-01-14 15:23:25 +0100

    Remove useless settings

--- vehicle/ragP/settings.lua
+++ vehicle/ragP/settings.lua
@@ -6,8 +6,4 @@ S_animation = "PUSHUP_END" -- Animation jouée au reveil du joueur.
 S_minP = 20 -- Pourcentage minimal de dégats infligés
 S_ratioDegats = 0.75 -- Multiplicateur appliqué à la vitesse du véhicule
 S_vitesseMin = 10 -- Vitesse minimale déclenchement ragdoll
-S_v1 = 50 -- Seuil du premier palier de vitesse
-S_v2 = 75 -- Seuil du premier palier de vitesse
-S_t2 = 15000 -- Temps en millisecondes de ragdoll appliqué au joueur sous le seuil du palier 2
-S_t3 = 30000 -- Temps en millisecondes de ragdoll appliqué au joueur au-dessus du seuil du palier 2
 
