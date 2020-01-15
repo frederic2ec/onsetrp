6f85466bc506b49bf63c3dc7783f855fc1d37eb4
GalaxHD551 <59099660+GalaxHD551@users.noreply.github.com>
2020-01-15 13:23:02 +0400

    Update client.lua

--- animation/client.lua
+++ animation/client.lua
@@ -13,28 +13,21 @@ end
 AddEvent("OnPackageStart", OnPackageStart)
 
 AddEvent("OnKeyPress", function(key)
-	if key == "G" and not alreadyInteracting then
-		if GetPlayerVehicle(GetPlayerId()) == 0 then
-			if(GetWebVisibility(animationWheelUI) == 0) then
+	if key == "G" and not IsPlayerInVehicle(GetPlayerId()) and not alreadyInteracting then
+		if(GetWebVisibility(animationWheelUI) == 0) then
 				local ScreenX, ScreenY = GetScreenSize()
 				SetMouseLocation(math.floor(ScreenX / 2), math.floor(ScreenY / 2))
 				SetWebVisibility(animationWheelUI, WEB_VISIBLE)
 				ShowMouseCursor(true)
 				SetInputMode(INPUT_GAMEANDUI)
-			else
+		else
 				SetWebVisibility(animationWheelUI, WEB_HIDDEN)
 				ShowMouseCursor(false)
 				SetInputMode(INPUT_GAME)
-			end
-		else
-			return CallRemoteEvent(player, "MakeNotification", _("cant_do_that"), "linear-gradient(to right, #ff5f6d, #ffc371)")
 		end
-	elseif key == "E" and not alreadyInteracting then
-		if GetPlayerVehicle(GetPlayerId()) == 0 then
-			CallRemoteEvent("PickupGun")
-		else
-			return CallRemoteEvent(player, "MakeNotification", _("cant_do_that"), "linear-gradient(to right, #ff5f6d, #ffc371)")
-		end
+
+	elseif key == "E" and not IsPlayerInVehicle(GetPlayerId()) and not alreadyInteracting then
+			CallRemoteEvent("PickupGun")		
 	end
 end)
 
