1c62829af3b545bb48440e0ca282d77c9d28c93a
GalaxHD551 <59099660+GalaxHD551@users.noreply.github.com>
2020-01-17 01:47:29 +0400

    Correct bug animation

--- misc/tazer.lua
+++ misc/tazer.lua
@@ -6,7 +6,7 @@ function OnTazerShot(player, weapon, hittype, hitid, hitx, hity, hitz, startx, s
 			SetPlayerRagdoll(hitid, true) -- Makes player ragdoll
             		Delay(6000, function() -- Waits 6 seconds before the player can stand up again
                 		SetPlayerRagdoll(hitid, false) -- Disables the ragdoll so he can walk again.
-                		SetPlayerAnimation(player, "PUSHUP_END")    
+                		SetPlayerAnimation(hitid, "PUSHUP_END")    
 			end)
 	    	end        
 	end
