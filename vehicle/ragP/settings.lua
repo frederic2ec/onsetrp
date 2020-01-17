2c00ed88b65a5423fee8ef983bd415de1183a8f9
Wheazy <55120311+Wheazy@users.noreply.github.com>
2020-01-18 08:25:05 +1030

    Update settings.lua
    Changed description language to English

--- vehicle/ragP/settings.lua
+++ vehicle/ragP/settings.lua
@@ -1,9 +1,9 @@
 -- Made with 🖤 By Bad
 -- https://github.com/Bad57/ragP
 
-S_ignoredVehicles = {"Helicopter_01","Helicopter_02"} -- ModelName des véhicules ignorés
-S_animation = "PUSHUP_END" -- Animation jouée au reveil du joueur.
-S_minP = 20 -- Pourcentage minimal de dégats infligés
-S_ratioDegats = 0.75 -- Multiplicateur appliqué à la vitesse du véhicule
-S_vitesseMin = 10 -- Vitesse minimale déclenchement ragdoll
+S_ignoredVehicles = {"Helicopter_01","Helicopter_02"} -- ModelName of ignored vehicles
+S_animation = "PUSHUP_END" -- Animation played when the player wakes up.
+S_minP = 20 -- Minimum percentage of damage inflicted
+S_ratioDegats = 0.75 -- Multiplier applied to vehicle speed
+S_vitesseMin = 10 -- Minimum ragdoll trigger speed
 
