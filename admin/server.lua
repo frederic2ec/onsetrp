604c4867edcbe6b1bcb2220e1bc6574466923d59
Soljian / Sheaven <maxverr@gmail.com>
2020-02-15 19:42:29 +0100

    tp to hospital

--- admin/server.lua
+++ admin/server.lua
@@ -31,7 +31,8 @@ local teleportPlace = {
     mining_process_1 = {-82629, 90991, 481},
     mining_process_2 = {-191437, -31107, 1148},
     mining_supplier = {67862, 184741, 535},
-    ironsmith = { -189805, -34122, 1148}
+    ironsmith = { -189805, -34122, 1148},
+    hospital = { 213530, 158344, 1416}
 }
 
 local weaponList = {
