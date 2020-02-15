acfa5698e2dbf77a6b64ec164e474cd603426e20
Soljian / Sheaven <maxverr@gmail.com>
2020-02-15 18:43:21 +0100

    dd

--- police/s_police.lua
+++ police/s_police.lua
@@ -438,8 +438,17 @@ function LaunchFriskPlayer(player, target)
     if PlayerData[player].job ~= "police" then return end
     if GetPlayerPropertyValue(target, "cuffed") ~= true then return end
     
+    local x, y, z = GetPlayerLocation(player)
+    local nearestPlayers = GetPlayersInRange3D(x, y, z, 200)
     local playerList = {}
-    table.insert(playerList, {id = k, name = PlayerData[target].accountid})
+    for k, v in pairs(nearestPlayers) do
+        if k ~= player then
+            --table.insert(playerList, {id = k, name = PlayerData[k].name})
+            local playerName
+            if PlayerData[k].accountid ~= nil and PlayerData[k].accountid ~= 0 then playerName = PlayerData[k].accountid else playerName = GetPlayerName(k) end            
+            table.insert(playerList, {id = k, name = playerName}) -- On prend le nom affiché (l'accountid)
+        end
+    end
     
     searchedPlayer = {id = tonumber(target), name = PlayerData[tonumber(target)].name, inventory = PlayerData[tonumber(target)].inventory}
     CallRemoteEvent(player, "OpenPersonalMenu", Items, PlayerData[player].inventory, PlayerData[player].name, player, playerList, GetPlayerMaxSlots(player), searchedPlayer)
