87e6cd62b73b86daef4c2e957192286ec462b1c8
Soljian / Sheaven <maxverr@gmail.com>
2020-02-23 02:40:46 +0100

    upgrade scoreboard

--- scoreboard/server.lua
+++ scoreboard/server.lua
@@ -1,31 +1,27 @@
 function Scoreboard_RequestUpdate(player)
-  local _send = {}
-  for _, v in ipairs(GetAllPlayers()) do
-    if PlayerData[v] == nil then
-        goto continue
+    local _send = {}
+    for _, v in ipairs(GetAllPlayers()) do
+        if PlayerData[v] ~= null and PlayerData[v].name ~= null and PlayerData[v].name ~= "" then
+            _send[v] = {
+                ['id'] = v,
+                ['accountid'] = PlayerData[v].accountid,
+                ['name'] = PlayerData[v].name or GetPlayerName(v),
+                ['steamid'] = tostring(GetPlayerSteamId(v)),
+                ['ping'] = GetPlayerPing(v)
+            }
+        end
     end
-    if PlayerData[v].name == nil then
-        goto continue
-    end
-    _send[v] = {
-      ['id'] = PlayerData[v].accountid,
-      ['name'] = PlayerData[v].name or GetPlayerName(v),
-      ['steamid'] = tostring(GetPlayerSteamId(v)),
-      ['ping'] = GetPlayerPing(v)
-    }
-    ::continue::
-  end
-
-  CallRemoteEvent(player, 'OnServerScoreboardUpdate', _send, GetServerName(), #GetAllPlayers(), GetMaxPlayers())
+    
+    CallRemoteEvent(player, 'OnServerScoreboardUpdate', _send, GetServerName(), #GetAllPlayers(), GetMaxPlayers())
 end
 AddRemoteEvent('RequestScoreboardUpdate', Scoreboard_RequestUpdate)
 
 AddRemoteEvent("scoreboard:server:isadmin", function(player)
-  local isAdmin
-  if PlayerData[player].admin == 1 then
-    isAdmin = true
-  else
-    isAdmin = false
-  end
-  CallRemoteEvent(player, "scoreboard:client:isadmin", isAdmin)  
+    local isAdmin
+    if PlayerData[player].admin == 1 then
+        isAdmin = true
+    else
+        isAdmin = false
+    end
+    CallRemoteEvent(player, "scoreboard:client:isadmin", isAdmin)
 end)
