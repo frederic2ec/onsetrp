4b790dceffdb017b926f44041b189c3ea3095d7f
Soljian <maxverr@gmail.com>
2020-02-26 09:10:37 +0100

    Woops, I was tired..

--- salary/server.lua
+++ salary/server.lua
@@ -14,15 +14,18 @@ CreateTimer(function()
             -- THOSE HAS TO BE ORDER BY WAGE AMOUNT (desc)
             if PlayerData[player].job == "medic" then
                 PlayerData[player].bank_balance = PlayerData[player].bank_balance + MEDIC_WAGE
+                CallRemoteEvent(player, "MakeSuccessNotification", _("salary_notification", _("price_in_currency", tostring(MEDIC_WAGE))))
             elseif PlayerData[player].job == "police" then
                 PlayerData[player].bank_balance = PlayerData[player].bank_balance + POLICE_WAGE
+                CallRemoteEvent(player, "MakeSuccessNotification", _("salary_notification", _("price_in_currency", tostring(POLICE_WAGE))))
             elseif PlayerData[player].job ~= nil then
                 PlayerData[player].bank_balance = PlayerData[player].bank_balance + JOB_WAGE
+                CallRemoteEvent(player, "MakeSuccessNotification", _("salary_notification", _("price_in_currency", tostring(JOB_WAGE))))
             else
                 PlayerData[player].bank_balance = PlayerData[player].bank_balance + MINIMUM_WAGE
+                CallRemoteEvent(player, "MakeSuccessNotification", _("salary_notification", _("price_in_currency", tostring(MINIMUM_WAGE))))
             end
     
-            CallRemoteEvent(player, "MakeSuccessNotification", _("salary_notification", _("price_in_currency", tostring(salary))))
         end
 	end
 end, minutes * 60000)
